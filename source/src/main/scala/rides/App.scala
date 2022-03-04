d0d715d5b259b203b6db58a98a2718ff77972d2c
Ethan Zimbelman <itszimbo@gmail.com>
2022-03-03 21:40:09 -0800

    create weather and ride objects from input

--- source/src/main/scala/rides/App.scala
+++ source/src/main/scala/rides/App.scala
@@ -8,7 +8,85 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
 
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.Instant
+
+case class Ride(
+  val distance: Float,
+  val cabType: String,
+  val day: String,
+  val hour: Int,
+  val destination: String,
+  val source: String,
+  val price: Double,
+  val surgeMultiplier: Double,
+  val name: String
+);
+
+case class Weather(
+  val temp: Float,
+  val location: String,
+  val clouds: Float,
+  val pressure: Float,
+  val rain: Float,
+  val day: String,
+  val hour: Int,
+  val humidity: Float,
+  val wind: Float
+);
+
 object App {
+  def doubleOrDefault(f: String, default: Double): Double = {
+    if (f == "") return default
+    return f.toDouble
+  }
+
+  def floatOrZero(f: String): Float = {
+    val default: Double = 0.0
+    if (f == "") return default.toFloat
+    return f.toFloat
+  }
+
+  def timestampToDayHour(timestamp: Long): (String, Int) = {
+    // ZonedDateTime object in Boston time (UTC-5)
+    // https://docs.oracle.com/javase/8/docs/api/java/time/ZonedDateTime.html
+    val dt = ZonedDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.of("UTC-5"))
+    return (dt.getDayOfWeek.toString, dt.getHour)
+  }
+
+  def parseRide(in: String): Ride = {
+    val s = in.split(",")
+
+    val distance = floatOrZero(s(0))
+    val cabType = s(1)
+    val time = timestampToDayHour(s(2).toLong)
+    val destination = s(3)
+    val source = s(4)
+    val price = doubleOrDefault(s(5), -1.0)
+    val surge = doubleOrDefault(s(6), 1.0)
+    val name = s(9)
+
+    val ride = Ride(distance, cabType, time._1, time._2, destination, source, price, surge, name)
+    return ride
+  }
+
+  def parseWeather(in: String): Weather = {
+    val s = in.split(",")
+
+    val temp = floatOrZero(s(0))
+    val location = s(1)
+    val clouds = floatOrZero(s(2))
+    val pressure = floatOrZero(s(3))
+    val rain = floatOrZero(s(4))
+    val time = timestampToDayHour(s(5).toLong)
+    val humidity = floatOrZero(s(6))
+    val wind = floatOrZero(s(7))
+
+    val weather = Weather(temp, location, clouds, pressure, rain, time._1, time._2, humidity, wind)
+    return weather
+  }
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -16,11 +94,11 @@ object App {
     val conf = new SparkConf().setAppName("Distributed ridesharing")
     val sc = new SparkContext(conf)
 
-    val rides = sc.textFile("./input/cab_rides.csv")
-    val weather = sc.textFile("./input/weather.csv")
+    val rides = sc.textFile("./input/cab_rides.csv").map(parseRide).filter(_.price > -1)
+    val weather = sc.textFile("./input/weather.csv").map(parseWeather)
 
     // extremely basic example of using weather data
-    val weatherCount = weather.count()
-    println(weatherCount)
+    weather.collect().foreach(println)
+    // rides.collect().foreach(println)
   }
 }
